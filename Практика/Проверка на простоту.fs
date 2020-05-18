a3a63ba1b927f25ec90b0abb0da2f03b17549287
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-05-18 10:56:13 +0300

    Add files via upload

---
+++ Практика/Проверка на простоту.fs
@@ -0,0 +1,20 @@
+﻿//Число на простоту.
+open System
+
+let prime n =
+    let rec prost n m = 
+        if n = m then true
+        else if n % m = 0 then false
+             else prost n (m + 1)
+    match n with
+    |1->false
+    |2->true
+    |_-> prost n 2    
+
+[<EntryPoint>]
+let main argv =
+    let value = System.Convert.ToInt32(System.Console.ReadLine()) 
+    let ans = prime value
+    System.Console.Write(ans)
+    let z = System.Console.ReadKey()
+    0
---
+++ Практика/Сумма цифр числа.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let rec func a = 
+    match a with
+    |0-> 0
+    |_-> (func (a / 10)) + (a % 10)
+
+[<EntryPoint>]
+let main argv =
+    let a = System.Convert.ToInt32(System.Console.ReadLine())
+    let A = func a
+    System.Console.Write(A)
+    let z = System.Console.ReadKey()
+    0
