5b3868eb40e2b7f6a662852a3c3bc206749b2851
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-10 00:27:12 +0300

    Попытка сделать проверку числа на простоту

---
+++ IZ12.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+// Проверка числа на простоту
+let thisPrime N =
+    let rec prime d =
+        if (N % d = 0 && d < N) then
+            false
+        else
+            if (d > N) then
+                true
+            else
+                prime (d+1)
+
+    if (N = 1) then
+        false
+    
+
+
+[<EntryPoint>]
+let main argv =
+    
+    let z = System.Console.ReadKey()
+    0
+
