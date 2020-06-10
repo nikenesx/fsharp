63b42c675952b78ddaf11b325419185ac392b03a
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-10 10:08:33 +0300

    Add files via upload

--- IZ12.fs
+++ IZ12.fs
@@ -1,24 +1,38 @@
 ﻿open System
 
 // Проверка числа на простоту
-let thisPrime N =
-    let rec prime d =
-        if (N % d = 0 && d < N) then
+let etoProst Y =
+    let rec prost d =
+        if (Y % d = 0 && d < Y) then
             false
         else
-            if (d > N) then
+            if (d > Y) then
                 true
             else
-                prime (d+1)
+                prost (d+1)
 
-    if (N = 1) then
+    if (Y = 1||Y=4)then
         false
-    
+    else
+        if (Y = 2) then
+            true
+        else
+            prost 3
 
+let nschalo (Y:int) =
+    let rec nachalo2 tekushiyDiv (list:int list) =
+        match tekushiyDiv with
+        | 0 -> list
+        | _ -> 
+            if (etoProst tekushiyDiv) then
+                nachalo2 (tekushiyDiv-1) 
+            else
+                nachalo2 (tekushiyDiv-1) list
+
+    nachalo2 (Y-1) []
 
 [<EntryPoint>]
 let main argv =
     
     let z = System.Console.ReadKey()
-    0
-
+    0  
