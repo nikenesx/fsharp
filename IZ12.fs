675f28b47972be330f9658b9b98a4d27f3f38160
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-10 10:57:58 +0300

    Add files via upload

--- IZ12.fs
+++ IZ12.fs
@@ -1,5 +1,12 @@
 ﻿open System
 
+// Добавляет в массив spisok div столько раз, сколько раз на него делится delstepen
+let rec delstepen tekushiyN div spisok =
+    if (tekushiyN % div = 0) then
+        delstepen (tekushiyN / div) div (div::spisok)
+    else
+        spisok
+
 // Проверка числа на простоту
 let etoProst Y =
     let rec prost d =
@@ -19,20 +26,24 @@ let etoProst Y =
         else
             prost 3
 
+// Строит заданный список для некоторого числа N
 let nschalo (Y:int) =
+    // Перебирает делители и заносит простые и их повторения в список
     let rec nachalo2 tekushiyDiv (list:int list) =
         match tekushiyDiv with
         | 0 -> list
         | _ -> 
             if (etoProst tekushiyDiv) then
-                nachalo2 (tekushiyDiv-1) 
+                nachalo2 (tekushiyDiv-1) (delstepen Y tekushiyDiv list)
             else
                 nachalo2 (tekushiyDiv-1) list
 
     nachalo2 (Y-1) []
 
+
 [<EntryPoint>]
 let main argv =
-    
+    do printf "%A" (nschalo (Convert.ToInt32(Console.ReadLine())))
     let z = System.Console.ReadKey()
     0  
+    
